619177a2e362797192a63e362bc59c173fad8b68
LXCuup <171059985@qq.com>
2018-04-20 19:03:24 +0800

    P340_python_repos.py

--- P340_python_repos.py
+++ P340_python_repos.py
@@ -1,13 +1,18 @@
+#-*- coding:utf-8 -*-
 import requests
-import json
-
 # 执行API调用并存储响应
-url = 'https://api.GitHub.com/search/repositories?q=language:python&sort=stars'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
-print("Status code:",r.Status_code)
+print("Status code:",r.status_code)
+
+
+
+
+
+
 
 # 将API响应存储在一个变量中
-response_dict = r.json()
+#response_dict = r.json()
 
 # 处理结果
-print(response_dict.keys())
+#print(response_dict.keys())
